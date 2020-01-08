8a12ecfe0f0a9d8580c65e7e7892af6a090720df
li3557024523 <3557024523@qq.com>
2020-01-08 15:14:02 +0800

    第四次bos

--- src/main/java/com/xr/bos/dao/Sor_StorageMapper.java
+++
@@ -1,4 +0,0 @@
-package com.xr.bos.dao;
-
-public interface Sor_StorageMapper {
-}
--- src/main/java/com/xr/bos/dao/dao.java
+++
@@ -1,7 +0,0 @@
-package com.xr.bos.dao;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class dao {
-}
