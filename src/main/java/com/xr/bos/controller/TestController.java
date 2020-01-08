982f9b8102168978e301006dbd5a7d717c7fae76
greg0618 <56573636+tgr0618@users.noreply.github.com>
2020-01-08 15:14:07 +0800

    第二次bos
    Signed-off-by: greg0618 <56573636+tgr0618@users.noreply.github.com>

--- src/main/java/com/xr/bos/controller/TestController.java
+++ src/main/java/com/xr/bos/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.xr.bos.controller;
 
+import com.xr.bos.mapper.SyEmpMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
+    @Autowired
+    public SyEmpMapper sy;
     @RequestMapping("/test")
     public ModelAndView test() {
         ModelAndView mv = new ModelAndView();
@@ -14,5 +18,10 @@ public class TestController {
         mv.addObject("test", a);
         mv.setViewName("pages/login.html");
         return mv;
+    }
+    @RequestMapping("/index")
+    public String ad(){
+        System.out.println(sy.queryOne(1).toString());
+        return sy.queryOne(1).toString();
     }
 }
